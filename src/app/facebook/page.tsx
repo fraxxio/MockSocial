feb47c0e38186091f57fe30f2ae44ec039371013
fraxxio <101990229+fraxxio@users.noreply.github.com>
2024-01-29 19:01:05 +0200

    Add routes

---
+++ src/app/facebook/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Facebok</div>;
+};
+
+export default page;
--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Cards from "@/lib/Cards.json";
 import Image from "next/image";
+import Link from "next/link";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const CardGrid = () => {
@@ -8,15 +9,20 @@ export const CardGrid = () => {
     <div className='grid gap-2  gap-y-6 grid-cols-4'>
       {Cards.map((UiCard) => {
         return (
-          <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
-            <CardHeader>
-              <CardTitle>{UiCard.title}</CardTitle>
-              <CardDescription>{UiCard.description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Image src={UiCard.img} alt='Example' width={700} height={700} />
-            </CardContent>
-          </Card>
+          <Link href={UiCard.route}>
+            <Card
+              key={UiCard.title}
+              className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'
+            >
+              <CardHeader>
+                <CardTitle>{UiCard.title}</CardTitle>
+                <CardDescription>{UiCard.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Image src={UiCard.img} alt='Example' width={700} height={700} />
+              </CardContent>
+            </Card>
+          </Link>
         );
       })}
     </div>
