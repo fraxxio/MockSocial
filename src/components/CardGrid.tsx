6cb84485a1c6264cfa1d7911168bddd13180e9d8
fraxxio <101990229+fraxxio@users.noreply.github.com>
2024-01-29 17:34:03 +0200

    Refactor CardGrid.tsx

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,98 +1,24 @@
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Facebook, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
+import Cards from "@/lib/Cards.json";
+import Image from "next/image";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const CardGrid = () => {
   return (
     <div className='grid gap-2  gap-y-6 grid-cols-4'>
-      <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
-        <CardHeader>
-          <CardTitle className='flex gap-2'>
-            {" "}
-            <Twitter /> Twitter / X
-          </CardTitle>
-          <CardDescription>Posts, comments, messages.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-      </Card>
-
-      <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
-        <CardHeader>
-          <CardTitle>Discord</CardTitle>
-          <CardDescription>Messages</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-      </Card>
-
-      <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
-        <CardHeader>
-          <CardTitle className='flex gap-2'>
-            <Facebook /> Facebook
-          </CardTitle>
-          <CardDescription>Posts, comments, messages.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-      </Card>
-
-      <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
-        <CardHeader>
-          <CardTitle className='flex gap-2'>
-            <Youtube />
-            Youtube
-          </CardTitle>
-          <CardDescription>Videos, community posts.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-      </Card>
-
-      <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
-        <CardHeader>
-          <CardTitle className='flex gap-2'>
-            <Linkedin /> LinkedIn
-          </CardTitle>
-          <CardDescription>Posts, messages, job listings.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-      </Card>
-
-      <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
-        <CardHeader>
-          <CardTitle className='flex gap-2'>
-            <Instagram /> Instagram
-          </CardTitle>
-          <CardDescription>Posts, messages, stories.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-      </Card>
-
-      <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
-        <CardHeader>
-          <CardTitle className='flex gap-2'>ChatGPT</CardTitle>
-          <CardDescription>Messages.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-      </Card>
+      {Cards.map((UiCard) => {
+        return (
+          <Card className='w-[300px] hover:border-gray-500 duration-150 cursor-pointer'>
+            <CardHeader>
+              <CardTitle>{UiCard.title}</CardTitle>
+              <CardDescription>{UiCard.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Image src={UiCard.img} alt='Example' width={700} height={700} />
+            </CardContent>
+          </Card>
+        );
+      })}
     </div>
   );
 };
